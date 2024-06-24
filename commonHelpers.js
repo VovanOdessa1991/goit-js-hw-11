aab72c0df5dc3a25ca567e70eb50fc28d8883ffc
VovanOdessa1991 <50461900+VovanOdessa1991@users.noreply.github.com>
2024-06-24 19:58:27 +0000

    Deploying to gh-pages from @ VovanOdessa1991/goit-js-hw-11@f6f7ae712981df104f873e11819fe619a7cce82c 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{S as a}from"./assets/vendor-10cb7c31.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const s of i.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function r(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function l(e){if(e.ep)return;e.ep=!0;const i=r(e);fetch(e.href,i)}})();function c(o,t,r,l,e){return`<li class="galery__item">
+import{i as c,S as d}from"./assets/vendor-0fc460d7.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const a of i.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&l(a)}).observe(document,{childList:!0,subtree:!0});function t(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function l(e){if(e.ep)return;e.ep=!0;const i=t(e);fetch(e.href,i)}})();function f(o,r,t,l,e){return`<li class="galery__item">
           <div class="galery__item-div">
             <a href="${o}">
               <img
@@ -15,11 +15,11 @@ import{S as a}from"./assets/vendor-10cb7c31.js";(function(){const t=document.cre
 
                 <li class="galeri__info-item">
                   <p class="galeri__info-header">Likes</p>
-                  <p class="galeri__ingo-footer">${t}</p>
+                  <p class="galeri__ingo-footer">${r}</p>
                 </li>
                 <li class="galeri__info-item">
                   <p class="galeri__info-header">Views</p>
-                  <p class="galeri__ingo-footer">${r}</p>
+                  <p class="galeri__ingo-footer">${t}</p>
                 </li>
                 <li class="galeri__info-item">
                   <p class="galeri__info-header">Comments</p>
@@ -32,5 +32,5 @@ import{S as a}from"./assets/vendor-10cb7c31.js";(function(){const t=document.cre
               </ul>
             </div>
           </div>
-        </li>`}function f(o){for(;o.firstChild;)o.removeChild(o.lastChild)}function d(o,t,r,l,e,i){const s=c(t,r,l,e,i);o.insertAdjacentHTML("afterbegin",s)}const n={formIMG:document.querySelector("form"),galery:document.querySelector(".galery__list")};n.formIMG.addEventListener("submit",g);function g(o){o.preventDefault(),console.log("IMG!");const t=o.target.name.value;console.log(t),p(t).then(r=>{if(r.hits.length==0){console.log("ЖОПА_КОТОСРОФА");return}console.log("length"+r.hits.length),console.log(r.hits),f(n.galery),r.hits.forEach(e=>{console.log(e.downloads),d(n.galery,e.webformatURL,e.likes,e.views,e.comments,e.downloads)}),new a(".galery__item-div a").on("show.simplelightbox",function(){})}),o.target.name.value=""}const u="https://pixabay.com/api/",_="?key=44446882-f589529ab68d1d31e6487214d";function p(o){const t=`&q=${o}&min_width=2450`,r=`${u}${_}${t}`;return fetch(r).then(e=>e.json()).catch(()=>{console.log("Error")})}
+        </li>`}function g(o){for(;o.firstChild;)o.removeChild(o.lastChild)}function u(o,r,t,l,e,i){const a=f(r,t,l,e,i);o.insertAdjacentHTML("afterbegin",a)}const s={formIMG:document.querySelector("form"),galery:document.querySelector(".galery__list"),loader:document.querySelector(".loader"),preLoader:document.querySelector(".preLoader")};n();s.formIMG.addEventListener("submit",m);function m(o){o.preventDefault(),console.log("IMG!");const r=o.target.name.value;console.log(r),L(),y(r).then(t=>{if(t.hits.length==0){console.log("ЖОПА_КОТОСРОФА"),n(),c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"});return}console.log("length"+t.hits.length),console.log(t.hits),g(s.galery),t.hits.forEach(e=>{console.log(e.downloads),u(s.galery,e.webformatURL,e.likes,e.views,e.comments,e.downloads)}),new d(".galery__item-div a").on("show.simplelightbox",function(){}),h()}),o.target.name.value=""}function h(){console.log("Лоадер!");const o=document.querySelectorAll("img, video");let r=0;console.log(o),Array.from(o).forEach(t=>{t.onload=()=>{r++,console.log(r),r===o.length-2&&(console.log("ZGRYZILOS!"),n())}})}const p="https://pixabay.com/api/",_="?key=44446882-f589529ab68d1d31e6487214d";function y(o){const r=`&q=${o}&min_width=2450`,l=`${p}${_}${r}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(l).then(i=>i.json()).catch(()=>{console.log("Error")})}function L(){s.loader.classList.remove("hidden"),s.preLoader.classList.remove("hidden")}function n(){s.loader.classList.add("hidden"),s.preLoader.classList.add("hidden")}
 //# sourceMappingURL=commonHelpers.js.map
