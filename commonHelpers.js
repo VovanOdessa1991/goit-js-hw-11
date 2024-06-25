f35351b3053fed7cd5d3dfe07af73c2c1e98048d
VovanOdessa1991 <50461900+VovanOdessa1991@users.noreply.github.com>
2024-06-25 16:52:57 +0000

    Deploying to gh-pages from @ VovanOdessa1991/goit-js-hw-11@1c24a461a9f23626e20df96055a0cb90c84ed955 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,9 @@
-import{i as c,S as d}from"./assets/vendor-0fc460d7.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const i of e)if(i.type==="childList")for(const a of i.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&l(a)}).observe(document,{childList:!0,subtree:!0});function t(e){const i={};return e.integrity&&(i.integrity=e.integrity),e.referrerPolicy&&(i.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?i.credentials="include":e.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function l(e){if(e.ep)return;e.ep=!0;const i=t(e);fetch(e.href,i)}})();function f(o,r,t,l,e){return`<li class="galery__item">
+import{i as c,S as f}from"./assets/vendor-0fc460d7.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function t(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function s(e){if(e.ep)return;e.ep=!0;const o=t(e);fetch(e.href,o)}})();function u(r,i,t,s,e){return`<li class="galery__item">
           <div class="galery__item-div">
-            <a href="${o}">
+            <a href="${r}">
               <img
                 class="galery-img"
-                src = "${o}"
+                src = "${r}"
                 alt=""
                 
                 
@@ -15,7 +15,7 @@ import{i as c,S as d}from"./assets/vendor-0fc460d7.js";(function(){const r=docum
 
                 <li class="galeri__info-item">
                   <p class="galeri__info-header">Likes</p>
-                  <p class="galeri__ingo-footer">${r}</p>
+                  <p class="galeri__ingo-footer">${i}</p>
                 </li>
                 <li class="galeri__info-item">
                   <p class="galeri__info-header">Views</p>
@@ -23,7 +23,7 @@ import{i as c,S as d}from"./assets/vendor-0fc460d7.js";(function(){const r=docum
                 </li>
                 <li class="galeri__info-item">
                   <p class="galeri__info-header">Comments</p>
-                  <p class="galeri__ingo-footer">${l}</p>
+                  <p class="galeri__ingo-footer">${s}</p>
                 </li>
                 <li class="galeri__info-item">
                   <p class="galeri__info-header">Downloads</p>
@@ -32,5 +32,5 @@ import{i as c,S as d}from"./assets/vendor-0fc460d7.js";(function(){const r=docum
               </ul>
             </div>
           </div>
-        </li>`}function g(o){for(;o.firstChild;)o.removeChild(o.lastChild)}function u(o,r,t,l,e,i){const a=f(r,t,l,e,i);o.insertAdjacentHTML("afterbegin",a)}const s={formIMG:document.querySelector("form"),galery:document.querySelector(".galery__list"),loader:document.querySelector(".loader"),preLoader:document.querySelector(".preLoader")};n();s.formIMG.addEventListener("submit",m);function m(o){o.preventDefault(),console.log("IMG!");const r=o.target.name.value;console.log(r),L(),y(r).then(t=>{if(t.hits.length==0){console.log("ЖОПА_КОТОСРОФА"),n(),c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"});return}console.log("length"+t.hits.length),console.log(t.hits),g(s.galery),t.hits.forEach(e=>{console.log(e.downloads),u(s.galery,e.webformatURL,e.likes,e.views,e.comments,e.downloads)}),new d(".galery__item-div a").on("show.simplelightbox",function(){}),h()}),o.target.name.value=""}function h(){console.log("Лоадер!");const o=document.querySelectorAll("img, video");let r=0;console.log(o),Array.from(o).forEach(t=>{t.onload=()=>{r++,console.log(r),r===o.length-2&&(console.log("ZGRYZILOS!"),n())}})}const p="https://pixabay.com/api/",_="?key=44446882-f589529ab68d1d31e6487214d";function y(o){const r=`&q=${o}&min_width=2450`,l=`${p}${_}${r}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(l).then(i=>i.json()).catch(()=>{console.log("Error")})}function L(){s.loader.classList.remove("hidden"),s.preLoader.classList.remove("hidden")}function n(){s.loader.classList.add("hidden"),s.preLoader.classList.add("hidden")}
+        </li>`}function d(r){r.innerHTML=""}function g(r,i,t,s,e,o){const a=u(i,t,s,e,o);r.insertAdjacentHTML("afterbegin",a)}const m="https://pixabay.com/api/",p="?key=44446882-f589529ab68d1d31e6487214d";function _(r){const i=`&q=${r}&min_width=2450`,s=`${m}${p}${i}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(s).then(o=>o.json()).catch(()=>{console.log("Error")})}const l={formIMG:document.querySelector("form"),galery:document.querySelector(".galery__list"),loader:document.querySelector(".loader"),preLoader:document.querySelector(".preLoader")};n();l.formIMG.addEventListener("submit",h);function h(r){if(r.preventDefault(),r.target.name.value.trim()===""){c.info({title:"Пустой РЯДОК!!"});return}const i=r.target.name.value;console.log(i),L(),_(i).then(t=>{if(t.hits.length==0){n(),c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),d(l.galery);return}d(l.galery),t.hits.forEach(e=>{g(l.galery,e.webformatURL,e.likes,e.views,e.comments,e.downloads)}),new f(".galery__item-div a").on("show.simplelightbox",function(){}),y()}),r.target.name.value=""}function y(){console.log("Лоадер!");const r=document.querySelectorAll("img, video");let i=0;console.log(r),Array.from(r).forEach(t=>{t.onload=()=>{i++,i===r.length-2&&(console.log("ZGRYZILOS!"),n())}})}function L(){l.loader.classList.remove("hidden"),l.preLoader.classList.remove("hidden")}function n(){l.loader.classList.add("hidden"),l.preLoader.classList.add("hidden")}
 //# sourceMappingURL=commonHelpers.js.map
